23d575209bb2f449e10ed16929d4264b33bd4b0b
Pavel Zrazheyski <termitspy@gmail.com>
2022-10-15 13:06:36 +0300

    поиск index нужного нам числа из массива данных

---
+++ example010_IntroArray_02/Program.cs
@@ -0,0 +1,18 @@
+﻿//                0   1   2    3   4   5   6   7   8
+int [] array = { 12, 52, 57, 455, 51, 16, 57, 88, 39};
+
+int n =array.Length;
+int find = 57;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index]  == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
